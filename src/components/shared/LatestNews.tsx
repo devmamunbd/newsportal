cdbeda0c6e028b63c670c9964a062bc0b100042b
devmamunbd <itsmemamunbd@gmail.com>
2025-02-05 21:00:11 +0800

    define news interface

--- src/components/shared/LatestNews.tsx
+++ src/components/shared/LatestNews.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import { Button } from "../ui/button";
 import team from '@/public/assets/BDTeam.jpeg'
 import Link from 'next/link'
+import { NewsItem } from "@/types/news";
 
 
 const LatestNews = async ()=> {
@@ -13,17 +14,21 @@ const LatestNews = async ()=> {
     return (
         <div>
             <h2 className="text-xl md:text-2xl text-black font-bold mb-3">Latest News</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 items-center">
-            <div className="bg-white rounded-md shadow-md px-4 py-3">
-            <Link href=""><Image
-            className="rounded-md hover:scale-105 cursor-pointer transition-all duration-200"
-            src={team} alt="Bangladesh Cricket Team" /></Link>
-            <div className="flex flex-col  mt-3">
-                <h2 className="text-xl font-bold mb-2">Bangladesh T20 National Cricket Team</h2>
-                <p className="text-sm text-gray-600 mb-4"> The Bangladesh National Cricket Team, known as the Tigers, represents Bangladesh in international cricket. </p>
-                <Link href=""><Button variant="default">Read More....</Button></Link>
-            </div>
-            </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 items-center">
+            {
+                news?.map((item: NewsItem)=> (
+                    <div key={item?._id} className="bg-white rounded-md shadow-md px-4 py-3">
+                    <Link href=""><Image
+                    className="rounded-md hover:scale-105 cursor-pointer transition-all duration-200"
+                    src={item?.imageUrl} alt="Bangladesh Cricket Team" width={500} height={450} /></Link>
+                    <div className="flex flex-col  mt-3">
+                        <h2 className="text-xl font-bold mb-2">{item?.title}</h2>
+                        <p className="text-sm text-gray-600 mb-4"> {item?.description}</p>
+                        <Link href=""><Button variant="default">Read More....</Button></Link>
+                    </div>
+                    </div>
+                ))
+            }
             </div>
         </div>
     )
